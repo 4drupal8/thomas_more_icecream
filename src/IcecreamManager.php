a95a57c70078f522707e578265f23ca08e5241ab
NeilVdB <neil2223@hotmail.com>
2018-05-24 15:09:14 +0200

    Mailfunctie

--- src/IcecreamManager.php
+++ src/IcecreamManager.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\thomas_more_icecream;
 
-use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Database\Connection;
 
 class IcecreamManager {
@@ -21,6 +20,35 @@ class IcecreamManager {
         'taste' => $taste,
         'topping' => $topping,
       ])->execute();
+
+    if (1 == 1) {
+      $mailManager = \Drupal::service('plugin.manager.mail');
+      $module = "thomas_more_icecream";
+      $key = 'send_mail';
+      $to = 'neil2223@hotmail.com';
+      $objecten = $this->getAllBestellingen('ijs');
+      $tekst = "<p>ijsjes</p>";
+      if (count($objecten) > 1) {
+        foreach ($objecten as $object) {
+          $tekst .= "<p>" . $object->taste . "</p>";
+        }
+      }
+      $params['message'] = $tekst;
+      $send = TRUE;
+      var_dump($this->getAllBestellingen('ijs'));
+
+      $result = $mailManager->mail($module, $key, $to, 'nl', $params, NULL, $send);
+      if ($result['result'] !== TRUE) {
+        drupal_set_message(t('propblem'), 'error');
+      }
+    }
+  }
+
+  public function getAllBestellingen(string $type) {
+    $query = $this->connection->select('thomas_more_icecream_counter', 't');
+    $query->condition('t.type', $type);
+    $query->fields('t');
+    return $query->execute()->fetchAll();
   }
 
   public function getAll(string $type) {
