4319babffec5ea034b22eec2cda4f1c6486f2778
NeilVdB <neil2223@hotmail.com>
2018-05-24 15:25:29 +0200

    Mail sturen

--- src/IcecreamManager.php
+++ src/IcecreamManager.php
@@ -20,28 +20,6 @@ class IcecreamManager {
         'taste' => $taste,
         'topping' => $topping,
       ])->execute();
-
-    if (1 == 1) {
-      $mailManager = \Drupal::service('plugin.manager.mail');
-      $module = "thomas_more_icecream";
-      $key = 'send_mail';
-      $to = 'neil2223@hotmail.com';
-      $objecten = $this->getAllBestellingen('ijs');
-      $tekst = "<p>ijsjes</p>";
-      if (count($objecten) > 1) {
-        foreach ($objecten as $object) {
-          $tekst .= "<p>" . $object->taste . "</p>";
-        }
-      }
-      $params['message'] = $tekst;
-      $send = TRUE;
-      var_dump($this->getAllBestellingen('ijs'));
-
-      $result = $mailManager->mail($module, $key, $to, 'nl', $params, NULL, $send);
-      if ($result['result'] !== TRUE) {
-        drupal_set_message(t('propblem'), 'error');
-      }
-    }
   }
 
   public function getAllBestellingen(string $type) {
